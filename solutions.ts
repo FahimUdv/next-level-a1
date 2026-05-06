a18f0ba179bc477fc5444972f65e2a0fe0d4bda4
unknown <shfahimudv2@gmail.com>
2026-05-07 01:23:08 +0600

    First 4 solutions are pushed

--- solutions.ts
+++ solutions.ts
@@ -1,8 +1,41 @@
-// Problem 01
+/*<<<<<<<<<<<<<<< Problem 01 >>>>>>>>>>>>>>>>*/
 
-const solution01 = (input: string): number => {
-  const lines = input.split('\n');
-  let sum = 0;
+function filterEvenNumbers(arr: number[]): number[] {
+    return arr.filter((n: number) => n % 2 === 0);
+}
+const myArr: number[] = [1, 2, 3, 4, 5, 6]
+const result = filterEvenNumbers(myArr);
+
+
+/*<<<<<<<<<<<<<<< Problem 02 >>>>>>>>>>>>>>>>*/
+
+const reverseString = (str: string): string => {
+    return str.split('').reverse().join('');
+}
+const str1: string = 'typescript';
+const result2 = reverseString(str1);
+
+
+/*<<<<<<<<<<<<<<< Problem 03 >>>>>>>>>>>>>>>>*/
+
+type StringOrNumber = number | string;
+const checkType = (input: StringOrNumber) => {
+    if(typeof input === 'number'){
+        return 'Number'
+    }else {
+        return 'String'
+    }
 }
 
-const.log(solution01(`your input here`));
+
+/*<<<<<<<<<<<<<<< Problem 04 >>>>>>>>>>>>>>>>*/
+
+const getProperty = <X, Y extends keyof X>(obj: X, key: Y): X[Y] => {
+    return obj[key];
+}
+const user = {
+    name: "Fahim",
+    age: 25,
+};
+
+const names = getProperty(user, "name"); 
